baf1ed2a9d2da5dab177385c8f18cb9006b4ac69
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-05 16:46:16 +0000

    task answer

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,14 +8,13 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
 	listint_t *fast = list;
-	listint_t *step = fast->next;
 
 	if (list == NULL)
 		return (0);
 	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
-		fast = step->next;
+		fast = fast->next->next;
 		if (fast == slow)
 			return (1);
 	}

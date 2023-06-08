5da7657e122066f42927173346bbb1519a39a9e7
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-08 17:28:48 +0000

    task answer

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,22 +4,22 @@ from calculator_1 import add, sub, mul, div
 
 
 def main():
-    args = sys.argv[1:]
-    a = int(args[0])
-    b = int(args[2])
-    if len(args) != 3:
+    args = sys.argv
+    if len(args) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    elif args[1] not in '+-*/':
+    a = int(args[1])
+    b = int(args[3])
+    if args[2] not in '+-*/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    elif args[1] == '+':
+    elif args[2] == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif args[1] == '-':
+    elif args[2] == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif args[1] == '*':
+    elif args[2] == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif args[1] == '/':
+    elif args[2] == '/':
         print("{} / {} = {}".format(a, b, div(a, b)))
 
 

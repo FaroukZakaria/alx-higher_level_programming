60b7ef6edc2f12a918c4a87026d0714092b9c961
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-08 16:00:27 +0000

    task answer

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,14 @@
 import importlib.util
 
 
-spec = importlib.util.spec_from_file_location("hidden_4", "hidden_4.pyc")
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
-names = [name for name in dir(module) if not name.startswith('_')]
-for name in names:
-    print(name)
+def main():
+    spec = importlib.util.spec_from_file_location("hidden_4", "hidden_4.pyc")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    names = [name for name in dir(module) if not name.startswith('_')]
+    for name in names:
+        print(name)
+
+
+if __name__ == '__main__':
+    main()

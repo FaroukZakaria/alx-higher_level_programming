10fb09b221be5327693727b44388656a68600d18
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-08 18:15:25 +0000

    task answer

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from magic_calculation_102  import add, sub
+
+
+
+add_result = add()
+sub_result = sub()
+if a < b:
+    c = add_result(a, b)
+    for i in range(4, 6):
+        c = add_result(c, i)
+    return(c)
+else:
+    return(sub_result(a, b))

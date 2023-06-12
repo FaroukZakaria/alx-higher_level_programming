b88d06ed24a5883ff44be9e66bcb67ffca1a028e
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-12 21:54:01 +0000

    task answer

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * is_palindrome - s
+ * @head: s
+ * Return: s
+ */
+int is_palindrome(listint_t **head)
+{
+	int i = 0, j = 0, k = 0;
+	listint_t **head_two = *head, **home = *head;
+
+	while (*head_two != NULL)
+	{
+		*head_two->next;
+		i++;
+	}
+	*head_two = *head;
+	for (j = 0; j < i - k; k++)
+	{
+

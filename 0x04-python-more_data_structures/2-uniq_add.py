c667a0bc9884c2c0dd8b6ebadcec5efca0507f0e
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-14 13:42:47 +0000

    task answer

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique = list(set(my_list))
-    i = 0
-    return(lambda x: i + x for x in unique)
+    y = 0
+    return(lambda x: x + y for x in unique)

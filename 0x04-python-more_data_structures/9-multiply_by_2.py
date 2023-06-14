15bc8440272e5d644f4244b37c63bac2845f42a4
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-14 15:14:25 +0000

    task answer

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        a_dictionary[i] *= 2
-    return (a_dictionary)
+    b = a_dictionary.copy()
+    for i in b:
+        b[i] *= 2
+    return (b)

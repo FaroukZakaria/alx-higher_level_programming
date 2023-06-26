448dd443494c9c55463be9a257e0ba5780c74051
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-26 07:04:22 +0000

    task answer

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,12 @@ def safe_print_list(my_list=[], x=0):
     for i in my_list:
         if a < x and i != my_list[-1]:
             try:
-                print(int(i))
+                print(i, end="")
                 a += 1
-            except:
-                a += 1
+            except ValueError:
+                continue
+            if a > x:
+                break
         else:
             print()
+    print()

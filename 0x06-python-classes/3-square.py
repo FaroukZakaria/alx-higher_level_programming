b24e71461077ebca4ace8ca77e0b6095b12f3155
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-27 23:21:58 +0000

    task answer

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,11 @@
 class Square:
     """Define Square"""
     def __init__(self, size=0):
-        """initialize size"""
+        """initialize size
+
+        Args:
+            size (int): s
+        """
         if isinstance(size, int) is not True:
             raise("size must be an integer")
         elif size < 0:

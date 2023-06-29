d1522f79f5c16f6ffc9c3601dc618dad2449aec8
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-06-29 07:23:40 +0000

    task answer

--- 0x06-python-classes/6-main.py
+++ 0x06-python-classes/6-main.py
@@ -14,17 +14,25 @@ print("--")
 my_square_3 = Square(3, (3, 0))
 my_square_3.my_print()
 
-print("--(lol)")
+print("--")
 
 mysquare = Square(3, (1, 1))
 print(mysquare.size)
 print(mysquare.area())
 print(mysquare.position)
 
-print("--")
+print("--lol7")
 
 my_square = Square(3, "Position")
 
-print("--")
+print("--lol8")
 
+my_square = Square(3, (1, ))
 
+print("--lol9")
+
+my_square = Square(3, (1, -3))
+
+print("--lol10")
+
+my_square = Square(3, (1, "3"))

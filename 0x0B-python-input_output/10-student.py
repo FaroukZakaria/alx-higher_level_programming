bd9b3be1553a251ace74b4fe499b04a1447b61a7
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-07-11 17:08:04 +0000

    task answer

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""S"""
+
+
+class Student:
+    """S"""
+    def __init__(self, first_name, last_name, age):
+        """S"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) is True and
+            isinstance(i, str) for i in attrs:
+                for j in attrs:
+                    if hasattrs(self, j) is True:
+                        getattr(self, j)
+                    return (j)
+        return (self.__dict__)

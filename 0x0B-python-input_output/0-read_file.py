fc4367cf994a9dbb817193b90cce4f970f17a99f
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-07-11 15:05:42 +0000

    task answer

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" s """
+
+
 def read_file(filename=""):
+    """function"""
     with open(filename, 'r', encoding='utf-8') as f:
         fp = f.read()
         print(fp, end="")

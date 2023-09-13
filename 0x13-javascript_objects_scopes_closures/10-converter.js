113d95ed7150ea58182b638377a4291242ce5614
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-13 07:49:39 +0000

    task answer

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  if (base === 0) {
-  return (0);
-  }
+  return function (num) {
+    if (base === 0) {
+      return (0);
+    }
 
-  let result = '';
-  while (num > 0) {
-    const remainder = number % base;
-    result = remainder.toString() + result;
-    number = Math.floor(number / base);
-  }
-  return (result);
-}
+    let result = '';
+    while (num > 0) {
+      const remainder = num % base;
+      result = remainder.toString() + result;
+      num = Math.floor(num / base);
+    }
+    return (result);
+  };
+};

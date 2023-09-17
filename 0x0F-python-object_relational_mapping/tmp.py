09f01f8d95e1c471c08b4e338e2b2c04f4185754
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-17 17:10:44 +0000

    task answer

---
+++ 0x0F-python-object_relational_mapping/tmp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" tmp """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host='localhost', user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cursor = db.cursor()
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    db.close()

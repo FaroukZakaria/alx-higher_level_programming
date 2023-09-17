2cb5e5f92f06bdb676b8399a824e1a752b95f96c
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-17 14:42:41 +0000

    task answer

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute("""
         SELECT * FROM states
-        WHERE name LIKE 'N%
+        WHERE name LIKE 'N%'
         ORDER BY states.id ASC
     """)
     results = cursor.fetchall()

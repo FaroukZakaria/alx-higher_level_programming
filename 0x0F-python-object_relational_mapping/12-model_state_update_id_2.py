1cc776392ef0baa6f73a0c356ab35af6634e414d
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-19 13:34:52 +0000

    task answer

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    update = session.query(State).filter_by(State.id=2).first()
+    update = session.query(State).filter_by(id=2).first()
     update.name = 'New Mexico'
     session.commit()

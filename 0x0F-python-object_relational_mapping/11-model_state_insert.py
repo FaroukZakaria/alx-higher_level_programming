e83d1da5b725e6687d680a933c40523fa0994a70
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-19 13:29:33 +0000

    task answer

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -12,6 +12,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
     results = session.query(State).filter_by(name='Louisiana').first()
     print(results.id)
     session.commit()

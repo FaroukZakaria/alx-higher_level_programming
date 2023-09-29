ae7e98e16ebbc77e82623b721481e8e12bf545e4
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-29 23:17:48 +0000

    task answer

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     url = "https://api.github.com/user"
     data = requests.get(url, headers={'Authorization': f'Bearer {password}'})
-    if data.status_code >= 400):
+    if data.status_code >= 400:
         print("None")
         exit()
     print(data.json()['id'])

f2eff4a95068e7dfbc2616aa6c29698470cbb736
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-29 21:58:22 +0000

    task answer

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Fetching URL """
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        content_str = content.decode('utf-8')
+        print("Body responese:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content_str))

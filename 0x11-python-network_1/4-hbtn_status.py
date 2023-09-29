9b4e14e74f588c464ef29addae4a4fe8512af232
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-29 22:46:41 +0000

    task answer

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,6 @@ import requests
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
-    print("Body response:\n\
-    \t- type: {}\n\
-    \t- content: {}".format(type(response.text), response.text))
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))

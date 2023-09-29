6fd0b4c9dec53df3fdd74794db7380a54616a034
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-29 23:26:40 +0000

    task answer

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     user = sys.argv[1]
     repo = sys.argv[2]
     limit = 10
-    url = f"https://api.github.com/repos/{user}/{repo}/commits"
-    commits = requests.get(url).json()
+    ur = f"https://api.github.com/repos/{user}/{repo}/commits?per_page={limit}"
+    commits = requests.get(ur).json()
     for commit in commits:
         name = commit.get("commit").get("author").get("name")
         print("{}: {}".format(commit.get("sha"), name))

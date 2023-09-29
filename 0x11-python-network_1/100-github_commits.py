2bb64193f683363b419e62560f50192258ff7745
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-29 23:25:13 +0000

    task answer

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     user = sys.argv[1]
     repo = sys.argv[2]
     limit = 10
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    url = f"https://api.github.com/repos/{user}/{repo}/commits"
     commits = requests.get(url).json()
     for commit in commits:
         name = commit.get("commit").get("author").get("name")

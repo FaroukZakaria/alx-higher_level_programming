82d1859a9fe180b0861784c3287695dd2fa677fa
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-29 22:38:41 +0000

    task answer

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" status #1 """
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print(
+    "Body response:\n"
+    "\t- type: {}\n"
+    "\t- content: {}\n"
+    .format(type(response.text), response.text)
+)

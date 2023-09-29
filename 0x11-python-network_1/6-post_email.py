c944428ed51b81897f5d72065b447d72c9de3ecf
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-29 22:57:15 +0000

    task answer

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,4 +7,4 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    print(requests.post(url, data={'email': email}))
+    print(requests.post(url, data={'email': email}).text)

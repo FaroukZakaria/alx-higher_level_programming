9fff32366bd6e1988c604e99e3687707cb74fc83
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-29 13:28:09 +0000

    task answer

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # cause a server to respond with a custom text ("You got me!")
-curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" \
-	--data-raw "You got me!" "0.0.0.0:5000/catch_me" -w "\n"
+curl -sLX PUT -H "Origin:School" -d "user_id=98" "0.0.0.0:5000/catch_me"

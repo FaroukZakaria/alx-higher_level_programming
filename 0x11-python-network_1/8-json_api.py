09983d2dd46e21dd278de7de363974dd23d30368
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-29 23:08:57 +0000

    task answer

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" API JSON TEST """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    q = "" if len(sys.argv) <= 1 else sys.argv[1]
+    url = "http://0.0.0.0:5000/search_user"
+    data = requests.post(url, data={'q': q})
+    try:
+        joined = data.join()
+        if joined:
+            print("[{}] {}".format(joined['id'], joined['name']))
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")

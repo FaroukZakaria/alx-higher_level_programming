4f621d30df702cd26a2875c98cd2bdc94a598685
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-09-29 23:09:59 +0000

    task answer

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     data = requests.post(url, data={'q': q})
     try:
-        joined = data.join()
+        joined = data.jsn()
         if joined:
             print("[{}] {}".format(joined['id'], joined['name']))
         else:

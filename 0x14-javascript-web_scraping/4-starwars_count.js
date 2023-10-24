790abafe7244e46d31cbbd7a3d76953ab2f71412
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-10-24 18:58:40 +0000

    task answer

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const req = require('request');
+const url = process.argv[2];
+const wedge = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+req(url, (error, response, body) => {
+  if (error) {
+    console.error(`${error.messaage}`);
+    return;
+  }
+  const data = JSON.parse(body);
+  let count = 0;
+  for (const key of data.results) {
+    count += key.characters.filter(char => char === wedge).length;
+    }
+  console.log(count);
+});

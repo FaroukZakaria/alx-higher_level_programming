d36360ce215bb654072aec58b8fd3d2f8d9f4852
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-11-02 13:45:57 +0000

    task answer

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$.ajax({url:'https://hellosalut.stefanbohacek.dev/?lang=fr',method:'GET',success:function(response){$('DIV#hello').text(response.hello);},error:function(error){console.error(error);}});});
